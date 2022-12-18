f03947603e7d87a2d593adde6d305c072eaa967b
“KrittawatMC” <tiees288@gmail.com>
2022-12-18 17:55:22 +0700

    Commit Table Users #1

--- pages/users.tsx
+++ pages/users.tsx
@@ -1,10 +1,34 @@
-import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
-// import styles from '../styles/Home.module.css'
+// import styles from '../styles/Users.module.css'
 import { useQuery, useMutation } from 'react-query'
 import axios from 'axios'
+
+import userService from '../services/users'
+
+// Material UI
+import { alpha } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+import TableSortLabel from '@mui/material/TableSortLabel';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
+import Checkbox from '@mui/material/Checkbox';
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Switch from '@mui/material/Switch';
+// import DeleteIcon from '@mui/icons-material/Delete';
+// import FilterListIcon from '@mui/icons-material/FilterList';
+import { visuallyHidden } from '@mui/utils';
 
 interface User {
   id?: number
@@ -13,61 +37,226 @@ interface User {
   PhoneNumber: string
 }
 
-export default function Home() {
+type Order = 'asc' | 'desc';
 
-  const [loading, setLoading] = useState(false)
-  const { data: userData, error, isError, refetch: refetchUser, isLoading } = useQuery(['UserData', {
-    name: 'Krittawat',
-    surname: 'Boo',
-    age: '25'
-  }],
-    async (query) => {
+interface HeadCell {
+  disablePadding: boolean;
+  id: keyof User;
+  label: string;
+  numeric: boolean;
+}
 
-      const token = localStorage.getItem('AuthT');
-      const params = query.queryKey?.[1]
-      const data = await axios.get('https://639ae512d514150197431f44.mockapi.io/api/v1//users')
+interface EnhancedTableProps {
+  numSelected: number;
+  onRequestSort: (event: React.MouseEvent<unknown>, property: keyof User) => void;
+  // onSelectAllClick: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  order: Order;
+  orderBy: string;
+  rowCount: number;
+}
 
-      // throw {error: 'Undefinded token'};
-
-      if (data.status == 200) {
-        // Some Stuff
-      }
-
-      return data?.data as User[]
-    }, {
-    retry: 0,
-    onError: (e) => {
-      console.log(e)
-      setLoading(true)
-    }
-  })
-
-  type UserData = {
-    name: string;
-    email: string;
-  };
-
-  const createUserFunct = async (data: User) => {
-    const response = await axios({
-      method: "POST",
-      url: 'https://639ae512d514150197431f44.mockapi.io/api/v1/users',
-      data: data,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-    return response.data
+function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
   }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
 
-  const { mutate: createUser, data: addUserResponse } = useMutation(createUserFunct, {
-    onSuccess: (success) => {
-      console.log(success)
-    }
-  })
+function getComparator<Key extends keyof User>(
+  order: Order,
+  orderBy: Key,
+): (
+  a: { [key in Key]: number | string },
+  b: { [key in Key]: number | string },
+) => number {
+  return order === 'desc'
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+const headCells: readonly HeadCell[] = [
+  {
+    id: 'id',
+    numeric: false,
+    disablePadding: true,
+    label: 'id',
+  },
+  {
+    id: 'name',
+    numeric: true,
+    disablePadding: false,
+    label: 'Name',
+  },
+  {
+    id: 'PhoneNumber',
+    numeric: true,
+    disablePadding: false,
+    label: 'Phone Number',
+  },
+  {
+    id: 'createdAt',
+    numeric: true,
+    disablePadding: false,
+    label: 'Created At',
+  },
+];
+
+function EnhancedTableHead(props: EnhancedTableProps) {
+  const { order, orderBy, numSelected, rowCount, onRequestSort } =
+    props;
+  const createSortHandler =
+    (property: keyof User) => (event: React.MouseEvent<unknown>) => {
+      onRequestSort(event, property);
+    };
 
   return (
-    <div>
+    <TableHead className=''>
+      <TableRow className=''>
+        {headCells.map((headCell) => (
+          <TableCell
+            key={headCell.id}
+            align='center'
+            // align={headCell.numeric ? 'right' : 'left'}
+            padding={headCell.disablePadding ? 'none' : 'normal'}
+            sortDirection={orderBy === headCell.id ? order : false}
+          >
+            <TableSortLabel
+              active={orderBy === headCell.id}
+              direction={orderBy === headCell.id ? order : 'asc'}
+              onClick={createSortHandler(headCell.id)}
+            >
+              {headCell.label}
+              {orderBy === headCell.id ? (
+                <Box component="span" sx={visuallyHidden}>
+                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                </Box>
+              ) : null}
+            </TableSortLabel>
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+}
+
+
+
+export default function Users() {
+
+  const { data: userData, isLoading, isFetching, refetch, status } = userService.FetchUser()
+  const { mutateAsync: createUser, isError } = userService.InserUser()
+
+  // State for Material UI
+  const [order, setOrder] = useState<Order>('asc');
+  const [orderBy, setOrderBy] = useState<keyof User>('id');
+  const [selected, setSelected] = useState<readonly string[]>([]);
+  const [page, setPage] = useState(0);
+  const [dense, setDense] = useState(false);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  const handleRequestSort = (
+    event: React.MouseEvent<unknown>,
+    property: keyof User,
+  ) => {
+    const isAsc = orderBy === property && order === 'asc';
+    setOrder(isAsc ? 'desc' : 'asc');
+    setOrderBy(property);
+  };
+
+  const handleChangePage = (event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const handleChangeDense = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setDense(event.target.checked);
+  };
+
+  // Avoid a layout jump when reaching the last page with empty rows.
+  const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - (userData?.length ?? 0)) : 0;
+
+  console.log(getComparator(order, 'id'))
+
+
+  const handleSubmit = async () => {
+    await createUser({ name: 'Krittawat333', PhoneNumber: '081203040404' })
+    if (!isError) {
+      refetch()
+    }
+  }
+
+  return (
+    <div style={{
+      margin: '0px 100px 20px 100px'
+    }}>
       <h1>List of Users - React Query</h1>
+      <div>
+        <Table
+          // sx={{ minWidth: 750 }}
+          aria-labelledby="tableTitle"
+          size={dense ? 'small' : 'medium'}
+        >
+          <EnhancedTableHead
+            numSelected={selected.length}
+            order={order}
+            orderBy={orderBy}
+            onRequestSort={handleRequestSort}
+            rowCount={(userData?.length ?? 0)}
+          />
+          <TableBody>
+            {/* if you don't need to support IE11, you can replace the `stableSort` call with:
+              rows.sort(getComparator(order, orderBy)).slice() */}
+            {userData?.sort(getComparator(order, orderBy))
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((row, index) => {
+                // const isItemSelected = isSelected(row.name);
+                const labelId = `enhanced-table-checkbox-${index}`;
+
+                return (
+                  <TableRow
+                    hover
+                    // onClick={(event) => handleClick(event, row.name)}
+                    role="checkbox"
+                    // aria-checked={isItemSelected}
+                    tabIndex={-1}
+                    key={row.name}
+                  // selected={isItemSelected}
+                  >
+                    <TableCell align="center">{row.id}</TableCell>
+                    <TableCell align="center">{row.name}</TableCell>
+                    <TableCell align="center">{row.PhoneNumber}</TableCell>
+                    <TableCell align="center">{row?.createdAt?.toString()}</TableCell>
+                  </TableRow>
+                );
+              })}
+            {emptyRows > 0 && (
+              <TableRow
+                style={{
+                  height: (dense ? 33 : 53) * emptyRows,
+                }}
+              >
+                <TableCell colSpan={6} />
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+        <TablePagination
+          rowsPerPageOptions={[5, 10, 25]}
+          component="div"
+          count={userData?.length ?? 0}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      </div>
       <ul>
         {
           userData?.map((user: User) => {
@@ -76,10 +265,16 @@ export default function Home() {
             )
           })
         }
+        {
+          (isLoading || isFetching) &&
+          <div>
+            Loading...
+          </div>
+        }
       </ul>
       <h1>Add New Users</h1>
       <button onClick={async () => {
-        const res = createUser({ name: 'Krittawat333', PhoneNumber: '081203040404' })
+        handleSubmit()
       }}>Add</button>
     </div>
   )
